f2ba3a8ff1c2be27b7890f83a91b2ce133777980
Ashiq <ashiqul.islam@sslwireless.com>
2021-09-23 00:30:51 +0600

    DB image byte array column added

--- app/src/main/java/com/shaon2016/meldcxandroidtest/data/local/db/entity/History.kt
+++ app/src/main/java/com/shaon2016/meldcxandroidtest/data/local/db/entity/History.kt
@@ -1,5 +1,6 @@
 package com.shaon2016.meldcxandroidtest.data.local.db.entity
 
+import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
@@ -7,5 +8,7 @@ import androidx.room.PrimaryKey
 data class History(
     @PrimaryKey(autoGenerate = true) val id: Int = 0,
     val url: String = "",
-    val time: String = ""
+    val time: String = "",
+    @ColumnInfo(name = "image_byte_array", typeAffinity = ColumnInfo.BLOB)
+    val imageByteArray : ByteArray = byteArrayOf(),
 )
